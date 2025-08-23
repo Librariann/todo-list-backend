4d83704993fe8963787c7469d066ffacc409387b
Librariann <okpc0306@naver.com>
2025-08-23 10:00:00 +0900

    Update UserRegistrationRequest with single name field and Korean phone regex

--- src/main/java/com/librarin/todo_list/user/dto/UserRegistrationRequest.java
+++ src/main/java/com/librarin/todo_list/user/dto/UserRegistrationRequest.java
@@ -32,12 +32,9 @@ public class UserRegistrationRequest {
     @NotBlank(message = "비밀번호 확인은 필수입니다")
     private String confirmPassword;
     
-    @Size(max = 50, message = "이름은 50자 이하여야 합니다")
-    private String firstName;
+    @Size(max = 100, message = "이름은 100자 이하여야 합니다")
+    private String name;
     
-    @Size(max = 50, message = "성은 50자 이하여야 합니다")
-    private String lastName;
-    
-    @Pattern(regexp = "^\\+?[1-9]\\d{1,14}$|^$", message = "올바른 전화번호 형식이 아닙니다")
+    @Pattern(regexp = "^(01[016789]\\d{7,8})?$", message = "올바른 전화번호 형식이 아닙니다")
     private String phoneNumber;
 }
