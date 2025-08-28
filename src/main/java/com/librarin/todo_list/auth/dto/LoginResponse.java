55c26d898fda330ab08ad12ba5f7440faf08e34b
Librariann <okpc0306@naver.com>
2025-08-28 10:00:00 +0900

    Remove refreshToken from LoginResponse for httpOnly cookie approach

--- src/main/java/com/librarin/todo_list/auth/dto/LoginResponse.java
+++ src/main/java/com/librarin/todo_list/auth/dto/LoginResponse.java
@@ -13,7 +13,6 @@ import lombok.*;
 public class LoginResponse {
     
     private String accessToken;
-    private String refreshToken;
     private String tokenType = "Bearer";
     private Long expiresIn;
     private String username;
