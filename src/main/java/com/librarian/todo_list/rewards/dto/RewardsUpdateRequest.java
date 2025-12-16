86af831cb14fc5466dc5016f4e1c2fd6995fa84d
Librariann <okpc0306@naver.com>
2025-12-16 12:00:00 +0900

    Update RewardsUpdateRequest 2025.12.16

--- src/main/java/com/librarian/todo_list/rewards/dto/RewardsUpdateRequest.java
+++ src/main/java/com/librarian/todo_list/rewards/dto/RewardsUpdateRequest.java
@@ -1,29 +1,18 @@
 package com.librarian.todo_list.rewards.dto;
 
 import com.librarian.todo_list.rewards.entity.Rewards;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Value;
 import org.hibernate.validator.constraints.Length;
 
 import java.io.Serializable;
 
-/**
- * DTO for {@link Rewards}
- */
 @Value
-public class RewardsRegistrationRequest implements Serializable {
-    @NotBlank
+public class RewardsUpdateRequest implements Serializable {
     @Length(min = 4, max = 50)
     String name;
-
-    @NotNull
     Rewards.RewardsType type;
-
-    @NotNull
     Integer point;
     String description;
     Boolean discount;
     Integer discountRate;
-    Boolean isActive;
 }
