7b56c38bcf308b49401ba5496641ec57bebef52e
Librariann <okpc0306@naver.com>
2025-12-17 10:00:10 +0900

    edit Login Logic

--- src/main/java/com/librarian/todo_list/auth/dto/LoginResponse.java
+++ src/main/java/com/librarian/todo_list/auth/dto/LoginResponse.java
@@ -20,6 +20,6 @@ public class LoginResponse {
     private String refreshToken;
     private String tokenType = "Bearer";
     private Long expiresIn;
-    private String username;
+    private String nickname;
     private String email;
 }
