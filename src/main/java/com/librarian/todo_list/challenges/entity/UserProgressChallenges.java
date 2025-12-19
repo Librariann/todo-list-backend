de68aad674cf9b9f8260e5213f48540ab8466fe4
Librariann <okpc0306@naver.com>
2025-12-19 10:00:10 +0900

    add Entity - Challenge, UserProgressChallenges

--- src/main/java/com/librarian/todo_list/challenges/entity/UserProgressChallenges.java
+++ src/main/java/com/librarian/todo_list/challenges/entity/UserProgressChallenges.java
@@ -1,4 +1,41 @@
 package com.librarian.todo_list.challenges.entity;
 
-public class UserProgressChallenges {
+import com.librarian.todo_list.common.entity.BaseEntity;
+import com.librarian.todo_list.points.entity.UserPoint;
+import com.librarian.todo_list.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Table(name = "user_progress_challenges")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class UserProgressChallenges extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "challenges_id", nullable = false)
+    private Challenges challenges;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private UserPoint.PeriodTypeStatus periodType;
+
+    @Column(nullable = false)
+    private String periodKey;
+
+    @Column(nullable = false)
+    private Integer currentCount;
+
+    @Column(nullable = false)
+    private Boolean isAchieved;
 }
