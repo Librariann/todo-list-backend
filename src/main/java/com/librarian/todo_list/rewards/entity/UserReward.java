06b4194bb7a394ca202bd14d2d5b33f83e610f8b
Librariann <okpc0306@naver.com>
2025-12-20 10:00:10 +0900

    add Entity userReward

--- src/main/java/com/librarian/todo_list/rewards/entity/UserReward.java
+++ src/main/java/com/librarian/todo_list/rewards/entity/UserReward.java
@@ -1,4 +1,28 @@
 package com.librarian.todo_list.rewards.entity;
 
-public class UserReward {
+import com.librarian.todo_list.common.entity.BaseEntity;
+import com.librarian.todo_list.user.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Table(name = "user_rewards")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class UserReward extends BaseEntity {
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "reward_id", nullable = false)
+    private Reward reward;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private boolean isActive = false;
 }
