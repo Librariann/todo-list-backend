3a80f0413ab93a7212bf75fbd5ccea11e22b64ba
Librariann <okpc0306@naver.com>
2025-12-20 10:00:10 +0900

    add Entity Todos

--- src/main/java/com/librarian/todo_list/todos/entity/Todo.java
+++
@@ -1,4 +0,0 @@
-package com.librarian.todo_list.todos.entity;
-
-public class Todo {
-}
---
+++ src/main/java/com/librarian/todo_list/todos/entity/Todos.java
@@ -0,0 +1,35 @@
+package com.librarian.todo_list.todos.entity;
+
+import com.librarian.todo_list.common.entity.BaseEntity;
+import com.librarian.todo_list.user.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Table(name = "todos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class Todo extends BaseEntity {
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private TodosStatus status = TodosStatus.READY;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private Integer orderIndex = 1;
+
+    public enum TodosStatus {
+        READY, PROCESS, DONE
+    }
+}
