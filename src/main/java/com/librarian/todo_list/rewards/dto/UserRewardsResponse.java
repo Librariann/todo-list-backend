55d2d34ded56a16dca02bbe071b1401b876e35f8
Librariann <okpc0306@naver.com>
2025-12-21 12:00:00 +0900

    Update UserRewardsResponse 2025.12.21

--- src/main/java/com/librarian/todo_list/rewards/dto/UserRewardsResponse.java
+++ src/main/java/com/librarian/todo_list/rewards/dto/UserRewardsResponse.java
@@ -1,8 +1,7 @@
 package com.librarian.todo_list.rewards.dto;
 
 import com.librarian.todo_list.rewards.entity.Rewards;
-import com.librarian.todo_list.user.dto.UserResponse;
-import com.librarian.todo_list.user.entity.User;
+import com.librarian.todo_list.rewards.entity.UserReward;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -12,34 +11,17 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class RewardsResponse {
+public class UserRewardsResponse {
 
     private Long id;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
-    private String name;
-    private Rewards.RewardsType type;
-    private Integer point;
-    private String description;
-    private Boolean discount;
-    private Integer discountRate;
-    private Boolean isActive;
 
-    /**
-     * Rewards 엔티티를 Response DTO로 변환하는 정적 메서드
-     */
-    public static RewardsResponse from(Rewards rewards) {
-        return RewardsResponse.builder()
-                .id(rewards.getId())
-                .createdAt(rewards.getCreatedAt())
-                .updatedAt(rewards.getUpdatedAt())
-                .name(rewards.getName())
-                .type(rewards.getType())
-                .point(rewards.getPoint())
-                .description(rewards.getDescription())
-                .discount(rewards.isDiscount())
-                .discountRate(rewards.getDiscountRate())
-                .isActive(rewards.isActive())
+    public static UserRewardsResponse from(UserReward userReward) {
+        return UserRewardsResponse.builder()
+                .id(userReward.getId())
+                .createdAt(userReward.getCreatedAt())
+                .updatedAt(userReward.getUpdatedAt())
                 .build();
     }
 }
