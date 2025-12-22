61bc6b61af0b91dea2253f9698738234d016e6c4
Librariann <okpc0306@naver.com>
2025-12-22 11:06:13 +0900

    add Entity - GoalProcess

--- src/main/java/com/librarian/todo_list/goals/entity/GoalProcess.java
+++ src/main/java/com/librarian/todo_list/goals/entity/GoalProcess.java
@@ -1,4 +1,44 @@
 package com.librarian.todo_list.goals.entity;
 
-public class GoalProcess {
+import com.librarian.todo_list.common.entity.BaseEntity;
+import com.librarian.todo_list.user.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "goal_process")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class GoalProcess extends BaseEntity {
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "goals_id", nullable = false)
+    private Goals goals;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private Integer periodIndex = 1;
+
+    @Column(nullable = false)
+    private LocalDate periodStart;
+
+    @Column(nullable = false)
+    private LocalDate periodEnd;
+
+    @Column(nullable = false)
+    private Integer currentCount;
+
+    @Column(nullable = false)
+    private Boolean isAchieved;
+
+    @Column(nullable = false)
+    private Boolean isFinalized;
 }
