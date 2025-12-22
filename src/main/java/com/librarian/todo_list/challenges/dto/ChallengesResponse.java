b28352e3eadad222285e41c34f338de7d0f75a46
Librariann <okpc0306@naver.com>
2025-12-22 12:00:00 +0900

    Update ChallengesResponse 2025.12.22

--- src/main/java/com/librarian/todo_list/challenges/dto/ChallengesResponse.java
+++ src/main/java/com/librarian/todo_list/challenges/dto/ChallengesResponse.java
@@ -1,7 +1,9 @@
 package com.librarian.todo_list.challenges.dto;
 
 import com.librarian.todo_list.challenges.entity.Challenges;
+import com.librarian.todo_list.points.entity.UserPoint;
 import com.librarian.todo_list.rewards.entity.Rewards;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -16,7 +18,13 @@ public class ChallengesResponse {
     private Long id;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
-
+    private String name;
+    private String description;
+    private String icon;
+    private UserPoint.PeriodTypeStatus recurrenceType;
+    private Integer targetCount;
+    private Integer point;
+    private Boolean isActive;
     /**
      * Rewards 엔티티를 Response DTO로 변환하는 정적 메서드
      */
@@ -25,6 +33,13 @@ public class ChallengesResponse {
                 .id(challenges.getId())
                 .createdAt(challenges.getCreatedAt())
                 .updatedAt(challenges.getUpdatedAt())
+                .name(challenges.getName())
+                .description(challenges.getDescription())
+                .icon(challenges.getIcon())
+                .recurrenceType(challenges.getRecurrenceType())
+                .targetCount(challenges.getTargetCount())
+                .point(challenges.getPoint())
+                .isActive(challenges.isActive())
                 .build();
     }
 }
