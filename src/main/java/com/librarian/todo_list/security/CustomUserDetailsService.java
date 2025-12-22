10cd884f8f511f61d0e130bb3dead988a4bd1db8
Librariann <okpc0306@naver.com>
2025-12-22 11:08:13 +0900

    modified CustomUserDetailService for Security and JWT

--- src/main/java/com/librarian/todo_list/security/CustomUserDetailsService.java
+++ src/main/java/com/librarian/todo_list/security/CustomUserDetailsService.java
@@ -20,14 +20,14 @@ import java.util.Collections;
 public class CustomUserDetailsService implements UserDetailsService {
     
     private final UserRepository userRepository;
-    
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username)
+        User user = userRepository.findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다: " + username));
-        
+
         return new org.springframework.security.core.userdetails.User(
-                user.getUsername(),
+                user.getEmail(),
                 user.getPassword(),
                 Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()))
         );
@@ -41,7 +41,7 @@ public class CustomUserDetailsService implements UserDetailsService {
                 .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다: " + id));
         
         return new org.springframework.security.core.userdetails.User(
-                user.getUsername(),
+                user.getNickname(),
                 user.getPassword(),
                 Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()))
         );
