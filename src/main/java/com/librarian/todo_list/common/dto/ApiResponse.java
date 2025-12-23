cfadd58cc906e9f8091d70b9869cb24f09364a6d
Librariann <okpc0306@naver.com>
2025-12-23 10:00:10 +0900

    modified ApiRepsonse contents

--- src/main/java/com/librarian/todo_list/common/dto/ApiResponse.java
+++ src/main/java/com/librarian/todo_list/common/dto/ApiResponse.java
@@ -1,5 +1,6 @@
 package com.librarian.todo_list.common.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@JsonFormat
 public class ApiResponse<T> {
     
     private boolean success;
