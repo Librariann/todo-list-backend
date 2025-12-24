19bc22af9f0a483054b027dcfc6c3d529a130a31
Librariann <okpc0306@naver.com>
2025-12-24 12:00:00 +0900

    Update UserProgressChallengesReq 2025.12.24

--- src/main/java/com/librarian/todo_list/challenges/dto/UserProgressChallengesReq.java
+++ src/main/java/com/librarian/todo_list/challenges/dto/UserProgressChallengesReq.java
@@ -1,7 +1,6 @@
 package com.librarian.todo_list.challenges.dto;
 
 import com.librarian.todo_list.points.entity.UserPoint;
-import com.librarian.todo_list.rewards.entity.Rewards;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Value;
@@ -10,7 +9,7 @@ import org.hibernate.validator.constraints.Length;
 import java.io.Serializable;
 
 @Value
-public class ChallengesRegistrationRequest implements Serializable {
+public class UserProgressChallengesReq implements Serializable {
 
     @NotBlank
     @Length(min = 4, max = 50)
