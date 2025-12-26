e516960fc23f784e14734bd3b974d66500be9162
Librariann <okpc0306@naver.com>
2025-12-26 12:00:00 +0900

    Update Challenges 2025.12.26

--- src/main/java/com/librarian/todo_list/challenges/entity/Challenges.java
+++ src/main/java/com/librarian/todo_list/challenges/entity/Challenges.java
@@ -1,10 +1,10 @@
 package com.librarian.todo_list.challenges.entity;
 
+import com.librarian.todo_list.challenges.dto.ChallengesUpdateRequest;
 import com.librarian.todo_list.common.entity.BaseEntity;
 import com.librarian.todo_list.points.entity.UserPoint;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.librarian.todo_list.rewards.dto.RewardsUpdateRequest;
+import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
@@ -20,22 +20,41 @@ public class Challenges extends BaseEntity {
     @Column(nullable = false)
     private String name;
 
-    @Column(nullable = false)
     private String description;
 
-    @Column(nullable = false)
     private String icon;
 
+    @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private UserPoint.PeriodTypeStatus recurrenceType;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private WorkType workType;
+
     @Column(nullable = false)
     private Integer targetCount;
+
+    @Column(nullable = false)
+    private Integer dailyMaxCount;
 
     @Column(nullable = false)
     private Integer point;
 
     @Column(nullable = false)
     @Builder.Default
-    private Boolean isActive = false;
+    private boolean isActive = false;
+
+    public enum WorkType {
+        HABITS, TODOS, GOALS
+    }
+
+    public void update(ChallengesUpdateRequest request){
+        if (request.getName() != null) this.name = request.getName();
+        if (request.getPoint() != null) this.point = request.getPoint();
+        if (request.getDescription() != null) this.description = request.getDescription();
+        if (request.getIcon() != null) this.icon = request.getIcon();
+        if (request.getTargetCount() != null) this.targetCount = request.getTargetCount();
+        if (request.getRecurrenceType() != null) this.recurrenceType = request.getRecurrenceType();
+    }
 }
