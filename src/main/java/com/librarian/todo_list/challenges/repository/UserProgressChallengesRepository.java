37c93557961ef3df5c71113ec15f3ebe6b04bc5c
Librariann <okpc0306@naver.com>
2025-12-28 12:00:00 +0900

    Update UserProgressChallengesRepository 2025.12.28

--- src/main/java/com/librarian/todo_list/challenges/repository/UserProgressChallengesRepository.java
+++ src/main/java/com/librarian/todo_list/challenges/repository/UserProgressChallengesRepository.java
@@ -1,7 +1,7 @@
 package com.librarian.todo_list.challenges.repository;
 
 import com.librarian.todo_list.challenges.entity.Challenges;
-import com.librarian.todo_list.rewards.entity.Rewards;
+import com.librarian.todo_list.challenges.entity.UserProgressChallenges;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,15 +9,5 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface ChallengesRepository extends JpaRepository<Challenges, Long> {
-   /*
-    * 보상목록 이름으로 조회
-    */
-    boolean existsByName(String name);
-    boolean existsByNameAndIsActiveTrueAndIdNot(String name, Long id);
-    //GetList Active true
-    List<Challenges> findByIsActiveTrue();
-
-    //GetOne Active true
-    Optional<Challenges> findByIdAndIsActiveTrue(Long id);
+public interface UserProgressChallengesRepository extends JpaRepository<UserProgressChallenges, Long> {
 }
