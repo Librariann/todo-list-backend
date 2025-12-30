40a756321d38cafa0467c528551d375c2e5ad672
Librariann <okpc0306@naver.com>
2025-12-30 12:00:00 +0900

    Update UserRewardsController 2025.12.30

--- src/main/java/com/librarian/todo_list/rewards/controller/UserRewardsController.java
+++ src/main/java/com/librarian/todo_list/rewards/controller/UserRewardsController.java
@@ -1,76 +1,56 @@
 package com.librarian.todo_list.rewards.controller;
 
 import com.librarian.todo_list.common.dto.ApiResponse;
-import com.librarian.todo_list.rewards.dto.RewardsRegistrationRequest;
-import com.librarian.todo_list.rewards.dto.RewardsResponse;
-import com.librarian.todo_list.rewards.dto.RewardsUpdateRequest;
+import com.librarian.todo_list.rewards.dto.*;
 import com.librarian.todo_list.rewards.service.RewardsService;
+import com.librarian.todo_list.rewards.service.UserRewardsService;
+import com.librarian.todo_list.security.CustomUserDetails;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/rewards")
+@RequestMapping("/api/user/rewards")
 @RequiredArgsConstructor
-public class RewardsController {
-    private final RewardsService rewardsService;
+public class UserRewardsController {
+    private final UserRewardsService userRewardsService;
 
     @GetMapping("/")
-    public ResponseEntity<ApiResponse<List<RewardsResponse>>> getRewardList() {
-        log.info("상품 목록 API 호출");
+    public ResponseEntity<ApiResponse<List<UserRewardsResponse>>> getUserRewardList() {
+        log.info("유저 상품 목록 API 호출");
 
-        List<RewardsResponse> rewardResponse = rewardsService.getRewards();
-
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(ApiResponse.success(rewardResponse, "보상 목록을 성공적으로 불러왔습니다."));
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<ApiResponse<RewardsResponse>> getOneReward(@PathVariable Long id) {
-        log.info("특정 상품 API 호출: id={}", id);
-
-        RewardsResponse rewardResponse = rewardsService.getOneReward(id);
+        List<UserRewardsResponse> rewardResponse = userRewardsService.getUserRewards();
 
         return ResponseEntity.status(HttpStatus.OK)
-                .body(ApiResponse.success(rewardResponse, "보상을 성공적으로 불러왔습니다."));
+                .body(ApiResponse.success(rewardResponse, "받은 보상 목록을 성공적으로 불러왔습니다."));
     }
 
-    @PostMapping("/register")
-    public ResponseEntity<ApiResponse<RewardsResponse>> registerReward(
-            @Valid @RequestBody RewardsRegistrationRequest request) {
-        log.info("상품 등록 API 호출: name={}", request.getName());
+    @PostMapping("/{rewardId}/redeem")
+    public ResponseEntity<ApiResponse<UserRewardsResponse>> redeemUserReward(
+            @PathVariable Long rewardId,
+        @AuthenticationPrincipal CustomUserDetails principal) {
+        log.info("유저 상품 등록 API 호출: rewardId={}", rewardId);
 
-        RewardsResponse rewardResponse = rewardsService.registerRewards(request);
+        UserRewardsResponse rewardResponse = userRewardsService.redeemUserRewards(rewardId, principal.getUser());
 
         return ResponseEntity.status(HttpStatus.CREATED)
-                .body(ApiResponse.success(rewardResponse, "보상이 성공적으로 등록 완료되었습니다."));
+                .body(ApiResponse.success(rewardResponse, "보상이 성공적으로 지급되었습니다"));
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<ApiResponse> updateReward(
-            @Valid @RequestBody RewardsUpdateRequest request,
-            @PathVariable Long id ) {
-        log.info("상품 수정 API 호출: name={}", request.getName());
+    public ResponseEntity<ApiResponse> useUserReward(@PathVariable Long id) {
+        log.info("유저 상품 사용 API 호출: id={}", id);
 
-        RewardsResponse rewardResponse = rewardsService.updateRewards(request, id);
+        UserRewardsResponse rewardResponse = userRewardsService.useUserRewards(id);
 
         return ResponseEntity.status(HttpStatus.OK)
-                .body(ApiResponse.success(rewardResponse, "보상이 성공적으로 수정 완료되었습니다."));
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<ApiResponse<RewardsResponse>> deleteReward(@PathVariable Long id) {
-        log.info("상품 삭제 API 호출: id={}", id);
-
-        RewardsResponse rewardResponse = rewardsService.deleteRewards(id);
-
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(ApiResponse.success(rewardResponse, "보상이 성공적으로 삭제 완료되었습니다."));
+                .body(ApiResponse.success(rewardResponse, "보상이 성공적으로 사용 완료되었습니다."));
     }
 }
