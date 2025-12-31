c5344f6e80a2f43bf5454f0ef16b4ae6b2914aba
Librariann <okpc0306@naver.com>
2025-12-31 12:00:00 +0900

    Fix rewards registration request naming

--- src/main/java/com/librarian/todo_list/rewards/dto/RewardsRegistrationRequest.java
+++ src/main/java/com/librarian/todo_list/rewards/dto/RewardsRegistrationRequest.java
@@ -2,7 +2,6 @@ package com.librarian.todo_list.rewards.dto;
 
 import com.librarian.todo_list.rewards.entity.Rewards;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Value;
 import org.hibernate.validator.constraints.Length;
@@ -13,7 +12,7 @@ import java.io.Serializable;
  * DTO for {@link Rewards}
  */
 @Value
-public class RewardsResgistrationRequest implements Serializable {
+public class RewardsRegistrationRequest implements Serializable {
     @NotBlank
     @Length(min = 4, max = 50)
     String name;
