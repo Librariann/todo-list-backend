d5d2bfce2db5e3568d8a6aac41bbc2c9ad21ddf5
Librariann <okpc0306@naver.com>
2026-01-01 12:00:00 +0900

    Update RewardsRepository 2026.01.01

--- src/main/java/com/librarian/todo_list/rewards/repository/RewardsRepository.java
+++ src/main/java/com/librarian/todo_list/rewards/repository/RewardsRepository.java
@@ -9,14 +9,8 @@ import java.util.Optional;
 
 @Repository
 public interface RewardsRepository extends JpaRepository<Rewards, Long> {
-   /*
-    * 보상목록 이름으로 조회
-    */
     boolean existsByName(String name);
     boolean existsByNameAndIsActiveTrueAndIdNot(String name, Long id);
-    //GetList Active true
     List<Rewards> findByIsActiveTrue();
-
-    //GetOne Active true
     Optional<Rewards> findByIdAndIsActiveTrue(Long id);
 }
