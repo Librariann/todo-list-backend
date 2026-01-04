9d4865327d867a0c2e202ed848f041100a384b7c
Librariann <okpc0306@naver.com>
2026-01-04 12:00:00 +0900

    Add rewards repository queries

--- src/main/java/com/librarian/todo_list/rewards/repository/RewardsRepository.java
+++ src/main/java/com/librarian/todo_list/rewards/repository/RewardsRepository.java
@@ -1,4 +1,19 @@
 package com.librarian.todo_list.rewards.repository;
 
-public class RewardsRepository {
+import com.librarian.todo_list.rewards.entity.Rewards;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface RewardsRepository extends JpaRepository<Rewards, Long> {
+   /*
+    * 보상목록 이름으로 조회
+    */
+    boolean existsByName(String name);
+
+    //GetActiveTrue
+    List<Rewards> findByIsActiveTrue();
 }
