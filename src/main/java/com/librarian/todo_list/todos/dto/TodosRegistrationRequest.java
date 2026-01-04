a40ea298c2b7451b980518c84a702a4a5a27d7b7
Librariann <okpc0306@naver.com>
2026-01-04 12:00:00 +0900

    Update TodosRegistrationRequest 2026.01.04

--- src/main/java/com/librarian/todo_list/todos/dto/TodosRegistrationRequest.java
+++ src/main/java/com/librarian/todo_list/todos/dto/TodosRegistrationRequest.java
@@ -1,26 +1,24 @@
-package com.librarian.todo_list.rewards.dto;
+package com.librarian.todo_list.todos.dto;
 
 import com.librarian.todo_list.rewards.entity.Rewards;
+import com.librarian.todo_list.todos.entity.Todos;
+import com.librarian.todo_list.user.entity.User;
+import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Value;
 import org.hibernate.validator.constraints.Length;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 
 @Value
-public class RewardsRegistrationRequest implements Serializable {
+public class TodosRegistrationRequest implements Serializable {
     @NotBlank
     @Length(min = 4, max = 50)
     String name;
 
     @NotNull
-    Rewards.RewardsType type;
-
-    @NotNull
-    Integer point;
-    String description;
-    Boolean discount;
-    Integer discountRate;
-    Boolean isActive;
+    LocalDate targetDate;
 }
