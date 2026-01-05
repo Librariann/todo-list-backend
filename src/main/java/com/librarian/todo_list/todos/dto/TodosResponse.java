4b0d316a732c59b9d77657b1856e9335cc7fbcb7
Librariann <okpc0306@naver.com>
2026-01-05 12:00:00 +0900

    Update TodosResponse 2026.01.05

--- src/main/java/com/librarian/todo_list/todos/dto/TodosResponse.java
+++ src/main/java/com/librarian/todo_list/todos/dto/TodosResponse.java
@@ -1,10 +1,15 @@
-package com.librarian.todo_list.rewards.dto;
+package com.librarian.todo_list.todos.dto;
 
 import com.librarian.todo_list.rewards.entity.Rewards;
-import com.librarian.todo_list.user.dto.UserResponse;
+import com.librarian.todo_list.todos.entity.Todos;
 import com.librarian.todo_list.user.entity.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.*;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Getter
@@ -12,34 +17,27 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class RewardsResponse {
+public class TodosResponse {
 
     private Long id;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
     private String name;
-    private Rewards.RewardsType type;
-    private Integer point;
-    private String description;
-    private Boolean discount;
-    private Integer discountRate;
-    private Boolean isActive;
-
+    private Todos.TodosStatus status;
+    private Integer orderIndex;
+    private LocalDate targetDate;
     /**
      * Rewards 엔티티를 Response DTO로 변환하는 정적 메서드
      */
-    public static RewardsResponse from(Rewards rewards) {
-        return RewardsResponse.builder()
-                .id(rewards.getId())
-                .createdAt(rewards.getCreatedAt())
-                .updatedAt(rewards.getUpdatedAt())
-                .name(rewards.getName())
-                .type(rewards.getType())
-                .point(rewards.getPoint())
-                .description(rewards.getDescription())
-                .discount(rewards.isDiscount())
-                .discountRate(rewards.getDiscountRate())
-                .isActive(rewards.isActive())
+    public static TodosResponse from(Todos todos) {
+        return TodosResponse.builder()
+                .id(todos.getId())
+                .createdAt(todos.getCreatedAt())
+                .updatedAt(todos.getUpdatedAt())
+                .name(todos.getName())
+                .status(todos.getStatus())
+                .orderIndex(todos.getOrderIndex())
+                .targetDate(todos.getTargetDate())
                 .build();
     }
 }
