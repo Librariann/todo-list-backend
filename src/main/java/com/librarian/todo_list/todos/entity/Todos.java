782bf637539c37f6f643e2e812ef33aa5889b9e1
Librariann <okpc0306@naver.com>
2026-01-07 12:00:00 +0900

    Update Todos 2026.01.07

--- src/main/java/com/librarian/todo_list/todos/entity/Todos.java
+++ src/main/java/com/librarian/todo_list/todos/entity/Todos.java
@@ -1,10 +1,14 @@
 package com.librarian.todo_list.todos.entity;
 
 import com.librarian.todo_list.common.entity.BaseEntity;
+import com.librarian.todo_list.rewards.dto.RewardsUpdateRequest;
+import com.librarian.todo_list.todos.dto.TodosUpdateRequest;
 import com.librarian.todo_list.user.entity.User;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "todos")
@@ -22,6 +26,7 @@ public class Todos extends BaseEntity {
     private String name;
 
     @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     @Builder.Default
     private TodosStatus status = TodosStatus.READY;
 
@@ -29,7 +34,15 @@ public class Todos extends BaseEntity {
     @Builder.Default
     private Integer orderIndex = 1;
 
+    @Column(nullable = false)
+    private LocalDate targetDate;
+
     public enum TodosStatus {
         READY, PROCESS, DONE
+    }
+
+    public void update(TodosUpdateRequest request){
+        if (request.getName() != null) this.name = request.getName();
+        if (request.getTargetDate() != null) this.targetDate = request.getTargetDate();
     }
 }
