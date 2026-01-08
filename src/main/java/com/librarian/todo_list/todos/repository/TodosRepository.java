c641eaab354e5536d0b106191e1101ce4c3639b1
Librariann <okpc0306@naver.com>
2026-01-08 12:00:00 +0900

    Update TodosRepository 2026.01.08

--- src/main/java/com/librarian/todo_list/todos/repository/TodosRepository.java
+++ src/main/java/com/librarian/todo_list/todos/repository/TodosRepository.java
@@ -1,22 +1,20 @@
-package com.librarian.todo_list.rewards.repository;
+package com.librarian.todo_list.todos.repository;
 
 import com.librarian.todo_list.rewards.entity.Rewards;
+import com.librarian.todo_list.todos.entity.Todos;
+import com.librarian.todo_list.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.swing.text.html.Option;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface RewardsRepository extends JpaRepository<Rewards, Long> {
-   /*
-    * 보상목록 이름으로 조회
-    */
+public interface TodosRepository extends JpaRepository<Todos, Long> {
     boolean existsByName(String name);
-    boolean existsByNameAndIsActiveTrueAndIdNot(String name, Long id);
-    //GetList Active true
-    List<Rewards> findByIsActiveTrue();
-
-    //GetOne Active true
-    Optional<Rewards> findByIdAndIsActiveTrue(Long id);
+    Optional<Todos> findTopByUserAndTargetDateOrderByOrderIndexDesc(User user, LocalDate targetDate);
+    boolean existsByNameAndIdNot(String name, Long id);
+    Optional<Todos> findByIdAndUserId(Long id, Long userId);
 }
