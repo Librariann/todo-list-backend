0ce630cc52301816f0b8834494e90893b2bac5ec
Librariann <okpc0306@naver.com>
2026-01-09 12:00:00 +0900

    Add ChallengesRepository 2026.01.09

---
+++ src/main/java/com/librarian/todo_list/challenges/repository/ChallengesRepository.java
@@ -0,0 +1,23 @@
+package com.librarian.todo_list.challenges.repository;
+
+import com.librarian.todo_list.challenges.entity.Challenges;
+import com.librarian.todo_list.rewards.entity.Rewards;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ChallengesRepository extends JpaRepository<Challenges, Long> {
+   /*
+    * 보상목록 이름으로 조회
+    */
+    boolean existsByName(String name);
+    boolean existsByNameAndIsActiveTrueAndIdNot(String name, Long id);
+    //GetList Active true
+    List<Rewards> findByIsActiveTrue();
+
+    //GetOne Active true
+    Optional<Rewards> findByIdAndIsActiveTrue(Long id);
+}
