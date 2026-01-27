03d7398f8a5cf942bd19bdb8785308d9d0dd00ed
Librariann <okpc0306@naver.com>
2026-01-27 10:00:00 +0900

    Add SwaggerConfig.java

---
+++ src/main/java/com/librarian/todo_list/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.librarian.todo_list.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Todo List API")
+                        .description("할 일 관리 및 챌린지 시스템을 제공하는 REST API")
+                        .version("v1.0.0"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+    }
+}
