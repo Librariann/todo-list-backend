3be26849b50758219e8a6405312f6ab997424129
Librariann <okpc0306@naver.com>
2026-03-01 12:00:00 +0900

    Fix query formatting in point repository

--- src/main/java/com/librarian/todo_list/points/repository/UserPointRepository.java
+++ src/main/java/com/librarian/todo_list/points/repository/UserPointRepository.java
@@ -11,11 +11,11 @@ import java.util.Optional;
 @Repository
 public interface UserPointRepository extends JpaRepository<UserPoint, Long> {
 
-    @Query("SELECT SUM(CASE WHEN up.action = com.librarian.todo_list.points.entity.UserPoint$ActionStatus.CREDIT THEN up.point ELSE -up.point END) " +
+    @Query("SELECT SUM(CASE WHEN up.action = com.librarian.todo_list.points.entity.UserPoint$ActionStatus.CREDIT THEN up.point ELSE - up.point END) " +
            "FROM UserPoint up WHERE up.user.id = :userId")
     Optional<Integer> calculateUserTotalPoints(@Param("userId") Long userId);
 
-    @Query("SELECT SUM(CASE WHEN up.action = com.librarian.todo_list.points.entity.UserPoint$ActionStatus.CREDIT THEN up.point ELSE -up.point END) " +
+    @Query("SELECT SUM(CASE WHEN up.action = com.librarian.todo_list.points.entity.UserPoint$ActionStatus.CREDIT THEN up.point ELSE - up.point END) " +
            "FROM UserPoint up WHERE up.user.id = :userId " +
            "AND up.periodType = :periodType AND up.periodKey = :periodKey")
     Optional<Integer> findPointsByUserAndPeriod(
