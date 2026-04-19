7d49b8046e4ee6114f2dc67475b5270abdb35816
Librariann <okpc0306@naver.com>
2026-04-19 12:00:00 +0900

    Update user rewards repository

--- src/main/java/com/librarian/todo_list/rewards/repository/UserRewardsRepository.java
+++ src/main/java/com/librarian/todo_list/rewards/repository/UserRewardsRepository.java
@@ -2,6 +2,7 @@ package com.librarian.todo_list.rewards.repository;
 
 import com.librarian.todo_list.rewards.entity.Rewards;
 import com.librarian.todo_list.rewards.entity.UserReward;
+import com.librarian.todo_list.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,6 +11,5 @@ import java.util.Optional;
 
 @Repository
 public interface UserRewardsRepository extends JpaRepository<UserReward, Long> {
-    //GetList Active true
-    List<UserReward> findByIsUsedTrue();
+    List<UserReward> findByUser(User user);
 }
